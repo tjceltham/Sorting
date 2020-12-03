86d7ae8b13dfa57471e50d18553cb12c5890f76c
Tony Collins <tjc@eltham-college.org.uk>
2020-12-03 10:51:24 +0000

    Sort task

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -6,7 +6,27 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+       
+            int[] scores = { 12, 1, 233, 4, 5, 55, 66, 7, 77, 88, 88, 8, 77 };
+
+            
+            // Print out the array in this order
+
+            // write a buble sort from high to low
+
+            // print out the array
+
+            // add an array of names(string) to corespond to each integer
+
+            // adapt the sort to swap the name and the score
+
+
+           //  Print out sort name and score
+
+           // the user be type in a name and the program should display that persons scores
+
+
+
 
 
         }
